dc1a3ab6392b4b7d9cd6ab3c0ba9906c7904835f
1020aiKi <133315048+1020aiKi@users.noreply.github.com>
2023-08-04 17:32:54 +0900

    Add files via upload

---
+++ kadai_004/Score_chapter04.java
@@ -0,0 +1,19 @@
+package kadai_004;
+
+public class Score_chapter04 {
+	public static void main(String[] args) {	
+	System.out.println("Aさん 10点");
+	System.out.println("Bさん 20点");
+	System.out.println("Cさん 30点");
+	System.out.println("Dさん 40点");
+	System.out.println("Eさん 50点");
+	System.out.println("Fさん 60点");
+	System.out.println("Gさん 70点");
+	System.out.println("Hさん 80点");
+	System.out.println("Iさん 90点");
+	System.out.println("Jさん 100点");
+	System.out.println("平均点は" + 550/10 +"点です。");
+	
+	}
+
+}
