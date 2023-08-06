99f2f45fbb77af577d4d65c729f9a51d16f73035
1020aiKi <songbenhaoping24@gmail.com>
2023-08-06 18:11:36 +0900

    課題

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -1,3 +1,21 @@
 package kadai_008;
 
-aaa
+public class Price_Chapter08{
+	public static void main(String[] args) {
+		int userAge =  30;
+		int serviceCost = 3000;
+		
+		switch(userAge) {
+		case 10 -> System.out.println(userAge + "代の料金は" + serviceCost + "円");
+		case 20 -> System.out.println(userAge + "代の料金は" + serviceCost + "円");
+		case 30 -> System.out.println(userAge + "代の料金は" + serviceCost + "円");
+		case 40 -> System.out.println(userAge + "代の料金は" + serviceCost + "円");
+		case 50 -> System.out.println(userAge + "代の料金は" + serviceCost + "円");
+		case 60 -> System.out.println(userAge + "代の料金は" + serviceCost + "円");
+		case 70 -> System.out.println(userAge + "代の料金は" + serviceCost + "円");
+		case 80 -> System.out.println(userAge + "代の料金は" + serviceCost + "円");
+		default -> System.out.println("上記以外の年代の料金は" + serviceCost + "円");
+		}
+		
+	}
+}
